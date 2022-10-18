ad4eb68a5fe67c9e5ba3c6d64c07c7e2fb9d57ca
Alexander Nischelwitzer <nischelwitzer@gmail.com>
2022-10-18 15:22:02 +0200

    DHT11 Example

---
+++ iot_dht11_v32/mqtt_secrets.h
@@ -0,0 +1,9 @@
+
+// FH DMT Broker
+
+#define MQTT_USER           "xxx"
+#define MQTT_PASS           "xxx"
+#define MQTT_BROKER         "dmt.xxx.at" 
+#define MQTT_CLIENT         "dmt01"
+
+#define MQTT_PORT           1883
